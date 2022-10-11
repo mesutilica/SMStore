2c96add63969dcb8632499ae860fc693ba726316
mesutilica <mesut.ilica@gmail.com>
2022-10-11 23:05:00 +0300

    11-10-2022 admin kategori resmi sil hariç tamam

--- SMStore.WebUI/Areas/Admin/Controllers/BrandsController.cs
+++ SMStore.WebUI/Areas/Admin/Controllers/BrandsController.cs
@@ -2,77 +2,50 @@
 using Microsoft.AspNetCore.Mvc;
 using SMStore.Entities;
 using SMStore.Service.Repositories;
+using SMStore.WebUI.Utils;
 
 namespace SMStore.WebUI.Areas.Admin.Controllers
 {
     [Area("Admin")]
-    public class AppUsersController : Controller
+    public class BrandsController : Controller
     {
-        private readonly IRepository<AppUser> _repository; // CRUD işlemleri için gerekli interface
+        private readonly IRepository<Brand> _repository;
 
-        public AppUsersController(IRepository<AppUser> repository)
+        public BrandsController(IRepository<Brand> repository)
         {
             _repository = repository;
         }
 
-        // GET: AppUsersController
+        // GET: BrandsController
         public async Task<ActionResult> Index()
         {
             var model = await _repository.GetAllAsync();
             return View(model);
         }
 
-        // GET: AppUsersController/Details/5
+        // GET: BrandsController/Details/5
         public ActionResult Details(int id)
         {
             return View();
         }
 
-        // GET: AppUsersController/Create
+        // GET: BrandsController/Create
         public ActionResult Create()
         {
             return View();
         }
 
-        // POST: AppUsersController/Create
+        // POST: BrandsController/Create
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<ActionResult> CreateAsync(AppUser entity)
+        public async Task<ActionResult> CreateAsync(Brand entity, IFormFile? Image)
         {
             if (ModelState.IsValid)
             {
                 try
                 {
+                    if (Image is not null) entity.Image = await FileHelper.FileLoaderAsync(Image); // FileLoaderAsync metodu bizim yazdığımız resim yükleme metodu
                     await _repository.AddAsync(entity);
-                    await _repository.SaveChangesAsync();
-                    return RedirectToAction(nameof(Index));
-                }
-                catch
-                {
-                    ModelState.AddModelError("","Hata Oluştu!");
-                }
-            }
-            
-            return View();
-        }
-
-        // GET: AppUsersController/Edit/5
-        public async Task<ActionResult> EditAsync(int id)
-        {
-            var model = await _repository.FindAsync(id);
-            return View(model);
-        }
-
-        // POST: AppUsersController/Edit/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<ActionResult> EditAsync(int id, AppUser appUser)
-        {
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _repository.Update(appUser);
                     await _repository.SaveChangesAsync();
                     return RedirectToAction(nameof(Index));
                 }
@@ -81,27 +54,55 @@ namespace SMStore.WebUI.Areas.Admin.Controllers
                     ModelState.AddModelError("", "Hata Oluştu!");
                 }
             }
-
-            return View();
+            return View(entity);
         }
 
-        // GET: AppUsersController/Delete/5
+        // GET: BrandsController/Edit/5
+        public async Task<ActionResult> EditAsync(int id)
+        {
+            var model = await _repository.FindAsync(id);
+            return View(model);
+        }
+
+        // POST: BrandsController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> EditAsync(int id, Brand entity, IFormFile? Image)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    if (Image is not null) entity.Image = await FileHelper.FileLoaderAsync(Image);
+                    _repository.Update(entity);
+                    await _repository.SaveChangesAsync();
+                    return RedirectToAction(nameof(Index));
+                }
+                catch
+                {
+                    ModelState.AddModelError("", "Hata Oluştu!");
+                }
+            }
+            return View(entity);
+        }
+
+        // GET: BrandsController/Delete/5
         public async Task<ActionResult> DeleteAsync(int id)
         {
             var model = await _repository.FindAsync(id);
             return View(model);
         }
 
-        // POST: AppUsersController/Delete/5
+        // POST: BrandsController/Delete/5
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<ActionResult> DeleteAsync(int id, AppUser appUser)
+        public async Task<ActionResult> DeleteAsync(int id, Brand entity)
         {
             try
             {
-                _repository.Delete(appUser);
+                _repository.Delete(entity);
                 await _repository.SaveChangesAsync();
-                return RedirectToAction("Index");
+                return RedirectToAction(nameof(Index));
             }
             catch
             {
---
+++ SMStore.WebUI/Utils/FileHelper.cs
@@ -0,0 +1,20 @@
+﻿namespace SMStore.WebUI.Utils
+{
+    public class FileHelper
+    {
+        public static async Task<string> FileLoaderAsync(IFormFile formFile, string filePath = "/wwwroot/Img/")
+        {
+            string fileName = ""; // yüklenecek dosya adı için değişken oluşturduk
+
+            fileName = formFile.FileName; // oluşturduğumuz değişkene yüklenecek dosya adını aktardık
+
+            string directory = Directory.GetCurrentDirectory() + filePath + fileName; // dosyanın yükleneceği dizini belirledik (GetCurrentDirectory metodu uygulamanın çalıştığı fiziksel yolu getirir)
+
+            using var stream = new FileStream(directory, FileMode.Create); // dosya yükleme için gerekli bir dosya akış nesnesi oluşturup sınıfa yükleme yapacağımız dizini(directory) ve yükleme tipimizi(yeni dosya oluşturma) belirttik
+
+            await formFile.CopyToAsync(stream); // yukardaki ayarlarla dosyamızı asenkron bir şekilde sunucuya yükledik
+
+            return fileName; // bu metodun kullanılacağı yere yüklenen dosya adını geri gönderdik
+        }
+    }
+}
